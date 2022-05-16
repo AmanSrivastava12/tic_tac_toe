f516004f3afb5b02a0ba0b8ddcd5d68b886659ba
AmanSrivastava12 <70564443+AmanSrivastava12@users.noreply.github.com>
2022-05-16 20:44:57 +0530

    Modified the JS file

--- tictac.js
+++ tictac.js
@@ -82,3 +82,68 @@ Grid.prototype.getFreeCellIndices = function () {
   }
   return resultArray;
 };
+
+Grid.prototype.getRowValues = function (index) {
+  if (index !== 0 && index !== 1 && index !== 2) {
+    console.error("Wrong arg for getRowValues!");
+    return undefined;
+  }
+  var i = index * 3;
+  return this.cells.slice(i, i + 3);
+};
+
+Grid.prototype.getRowIndices = function (index) {
+  if (index !== 0 && index !== 1 && index !== 2) {
+    console.error("Wrong arg for getRowIndices!");
+    return undefined;
+  }
+  var row = [];
+  index = index * 3;
+  row.push(index);
+  row.push(index + 1);
+  row.push(index + 2);
+  return row;
+};
+
+Grid.prototype.getColumnValues = function (index) {
+  if (index !== 0 && index !== 1 && index !== 2) {
+    console.error("Wrong arg for getColumnValues!");
+    return undefined;
+  }
+  var i,
+    column = [];
+  for (i = index; i < this.cells.length; i += 3) {
+    column.push(this.cells[i]);
+  }
+  return column;
+};
+
+Grid.prototype.getColumnIndices = function (index) {
+  if (index !== 0 && index !== 1 && index !== 2) {
+    console.error("Wrong arg for getColumnIndices!");
+    return undefined;
+  }
+  var i,
+    column = [];
+  for (i = index; i < this.cells.length; i += 3) {
+    column.push(i);
+  }
+  return column;
+};
+
+Grid.prototype.getDiagValues = function (arg) {
+  var cells = [];
+  if (arg !== 1 && arg !== 0) {
+    console.error("Wrong arg for getDiagValues!");
+    return undefined;
+  } else if (arg === 0) {
+    cells.push(this.cells[0]);
+    cells.push(this.cells[4]);
+    cells.push(this.cells[8]);
+  } else {
+    cells.push(this.cells[2]);
+    cells.push(this.cells[4]);
+    cells.push(this.cells[6]);
+  }
+  return cells;
+};
