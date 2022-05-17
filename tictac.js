bc6eb6f3ae3987abf1d0f4d603fdea0c6e3cb9ab
AmanSrivastava12 <70564443+AmanSrivastava12@users.noreply.github.com>
2022-05-17 19:33:55 +0530

    Modified the JS file

--- tictac.js
+++ tictac.js
@@ -147,3 +147,70 @@ Grid.prototype.getDiagValues = function (arg) {
   }
   return cells;
 };
+Grid.prototype.getDiagIndices = function (arg) {
+  if (arg !== 1 && arg !== 0) {
+    console.error("Wrong arg for getDiagIndices!");
+    return undefined;
+  } else if (arg === 0) {
+    return [0, 4, 8];
+  } else {
+    return [2, 4, 6];
+  }
+};
+
+Grid.prototype.getFirstWithTwoInARow = function (agent) {
+  if (agent !== computer && agent !== player) {
+    console.error(
+      "Function getFirstWithTwoInARow accepts only player or computer as argument."
+    );
+    return undefined;
+  }
+  var sum = agent * 2,
+    freeCells = shuffleArray(this.getFreeCellIndices());
+  for (var i = 0; i < freeCells.length; i++) {
+    for (var j = 0; j < 3; j++) {
+      var rowV = this.getRowValues(j);
+      var rowI = this.getRowIndices(j);
+      var colV = this.getColumnValues(j);
+      var colI = this.getColumnIndices(j);
+      if (sumArray(rowV) == sum && isInArray(freeCells[i], rowI)) {
+        return freeCells[i];
+      } else if (sumArray(colV) == sum && isInArray(freeCells[i], colI)) {
+        return freeCells[i];
+      }
+    }
+    for (j = 0; j < 2; j++) {
+      var diagV = this.getDiagValues(j);
+      var diagI = this.getDiagIndices(j);
+      if (sumArray(diagV) == sum && isInArray(freeCells[i], diagI)) {
+        return freeCells[i];
+      }
+    }
+  }
+  return false;
+};
+
+Grid.prototype.reset = function () {
+  for (var i = 0; i < this.cells.length; i++) {
+    this.cells[i] = 0;
+  }
+  return true;
+};
+
+function initialize() {
+  myGrid = new Grid();
+  moves = 0;
+  winner = 0;
+  gameOver = false;
+  whoseTurn = player;
+  for (var i = 0; i <= myGrid.cells.length - 1; i++) {
+    myGrid.cells[i] = 0;
+  }
+  setTimeout(showOptions, 500);
+}
+
+function assignRoles() {
+  askUser("Do you want to go first?");
+  document.getElementById("yesBtn").addEventListener("click", makePlayerX);
+  document.getElementById("noBtn").addEventListener("click", makePlayerO);
+}
